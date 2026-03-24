cd94287ab3611acfb319145b7d094397e40eb4e6
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Pro.local>
2026-03-24 09:12:09 +0530

    UC2: Room Types

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
+class Room {
+    int id;
+    String type;
+    boolean available = true;
+
+    Room(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+}
+
 public class BookMyStayApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay App");
+
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(new Room(101, "Single"));
+        rooms.add(new Room(102, "Double"));
+
+        for(Room r : rooms) {
+            System.out.println(r.id + " " + r.type + " Available: " + r.available);
+        }
     }
 }
